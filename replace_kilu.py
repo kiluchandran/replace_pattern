7cdb9207ba9585ce71e630e5d08605c2b5af98ec
kiluchandran <kiluchandran@gmail.com>
2024-02-13 15:43:14 +0530

    Read filename,word to be searched and word to be replaced with from the command line arguments using argparse module

--- replace_kilu.py
+++ replace_kilu.py
@@ -1,12 +1,17 @@
 import argparse
 
-parser = argparse.ArgumentParser
+parser = argparse.ArgumentParser()
 parser.add_argument("input_file")
+parser.add_argument("-f", "--find")
+parser.add_argument("-r", "--replace")
 args = parser.parse_args()
 
 
 def main():
-    pass
+    file_name = args.input_file
+    if args.find and args.replace:
+        search_word = args.find
+        replace_word = args.replace
 
 
 main()
