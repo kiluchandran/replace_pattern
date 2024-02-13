8d53b0a3182ab4ac295349656b9a9de66b8cbc5e
kiluchandran <kiluchandran@gmail.com>
2024-02-13 17:16:41 +0530

    Made some changes.word to be searched and word to be replaced with are given as positional arguments in the command line

--- replace_kilu.py
+++ replace_kilu.py
@@ -2,8 +2,10 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("input_file")
-parser.add_argument("-f", "--find")
-parser.add_argument("-r", "--replace")
+parser.add_argument("search_word")
+parser.add_argument("replace_word")
+parser.add_argument("-f", "--find", action="store_true")
+parser.add_argument("-r", "--replace", action="store_true")
 args = parser.parse_args()
 
 
@@ -13,12 +15,15 @@ def search_replace(file_name, search_word, replace_word):
         updated_contents = file_contents.replace(search_word, replace_word)
         print(updated_contents)
 
+    # with open(file_name, "w") as file:
+    #     file.write(updated_contents)
+
 
 def main():
     file_name = args.input_file
     if args.find and args.replace:
-        search_word = args.find
-        replace_word = args.replace
+        search_word = args.search_word
+        replace_word = args.replace_word
         search_replace(file_name, search_word, replace_word)
 
 
