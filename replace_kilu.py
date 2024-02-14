6492eb4caef1625040e28b558adefbaf1e9bd4aa
kiluchandran <kiluchandran@gmail.com>
2024-02-14 20:26:09 +0530

    Removed optional arguments

--- replace_kilu.py
+++ replace_kilu.py
@@ -4,8 +4,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("input_file")
 parser.add_argument("search_word")
 parser.add_argument("replace_word")
-parser.add_argument("-f", "--find", action="store_true")
-parser.add_argument("-r", "--replace", action="store_true")
 args = parser.parse_args()
 
 
@@ -15,16 +13,12 @@ def search_replace(file_name, search_word, replace_word):
         updated_contents = file_contents.replace(search_word, replace_word)
         print(updated_contents)
 
-    # with open(file_name, "w") as file:
-    #     file.write(updated_contents)
-
 
 def main():
     file_name = args.input_file
-    if args.find and args.replace:
-        search_word = args.search_word
-        replace_word = args.replace_word
-        search_replace(file_name, search_word, replace_word)
+    search_word = args.search_word
+    replace_word = args.replace_word
+    search_replace(file_name, search_word, replace_word)
 
 
 main()
